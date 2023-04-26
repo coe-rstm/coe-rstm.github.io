450d7d63543beb83e7d2eaf59d6e1faa483beb58
Sharanjit Singh <Sharanjitsinghsyan@gmail.com>
2023-04-27 00:26:55 +0530

    updates activities

---
+++ wp-admin/post.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=httproad.in/wp-login.php?redirect_to=http%3A%2F%2Froad.in%2Fwp-admin%2Fpost.php&reauth=1">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "httproad.in/wp-login.php?redirect_to=http%3A%2F%2Froad.in%2Fwp-admin%2Fpost.php&reauth=1";
+		</script>
+
+		<p>You are being redirected to <a href="httproad.in/wp-login.php?redirect_to=http%3A%2F%2Froad.in%2Fwp-admin%2Fpost.php&reauth=1">httproad.in/wp-login.php?redirect_to=http%3A%2F%2Froad.in%2Fwp-admin%2Fpost.php&reauth=1</a></p>
+	</body>
+</html>
