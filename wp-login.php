3fe8d34923139f947e4b422c70343af07add355d
Sharanjit Singh <Sharanjitsinghsyan@gmail.com>
2023-04-27 23:23:11 +0530

    update

--- wp-login.php
+++ wp-login.php
@@ -79,7 +79,7 @@ var _wpUtilSettings = {"ajax":{"url":"\/wp-admin\/admin-ajax.php"}};
 </script>
 <script src="/wp-includes/js/wp-util.min.js?ver=6.2" id="wp-util-js"></script>
 <script id="user-profile-js-extra">
-var userProfileL10n = {"user_id":"0","nonce":"54c56e4890"};
+var userProfileL10n = {"user_id":"0","nonce":"61485624cf"};
 </script>
 <script src="/wp-admin/js/user-profile.min.js?ver=6.2" id="user-profile-js"></script>
 	<div class="clear"></div>
